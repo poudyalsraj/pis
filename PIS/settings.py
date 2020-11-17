128689761218e8a35c8d28134b02126d8c546ebb
unknown <poudyalsraj@gmail.com>
2020-11-17 21:54:32 +0545

    Debug True

--- PIS/settings.py
+++ PIS/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(yln!^faw_+yxg9e1+@za9^nm1tb21z)0a^ua03-&))*$3tfvi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['pis1.herokuapp.com', '127.0.0.1']
-
 
 # Application definition
 
